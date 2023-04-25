9a16a978b4a40da80d5f00a907c8363327fa9e92
Jayzing00 <jayleo2106@gmail.com>
2023-04-25 13:43:09 +0200

    cleanup

--- mdm_proj2/src/main/java/zhaw/nageljay/mdm_proj2/HuggingFaceApiClient.java
+++ mdm_proj2/src/main/java/zhaw/nageljay/mdm_proj2/HuggingFaceApiClient.java
@@ -5,7 +5,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.util.Properties;
